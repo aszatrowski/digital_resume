cb41d2d822af2269ade282ea9afcd97696606a8f
aszatrowski <77942205+aszatrowski@users.noreply.github.com>
2021-02-06 00:30:35 -0500

    all column1 subheadings finished, minus activities

--- scripts/main.js
+++ scripts/main.js
@@ -6,31 +6,27 @@ document.getElementById("contact").onclick = function() {
   if (contactDisplayStatus == "none") {
       contactDisplayStatus = "block";
       document.getElementById("contact-info").style.display = contactDisplayStatus;
-      console.log("showing contact");
   }
   else if (contactDisplayStatus == "block") {
       contactDisplayStatus = "none";
       document.getElementById("contact-info").style.display = contactDisplayStatus;
-      console.log("hiding contact");
   }
 }
 
-// EDUCATION
-// let educationDisplayStatus = "none";
-// document.getElementById("contact-info").style.display = educationDisplayStatus;
-//
-// document.getElementById("contact").onclick = function() {
-//   if (educationDisplayStatus == "none") {
-//       educationDisplayStatus = "block";
-//       document.getElementById("contact-info").style.display = educationDisplayStatus;
-//       console.log("showing");
-//   }
-//   else if (educationDisplayStatus == "block") {
-//       educationDisplayStatus = "none";
-//       document.getElementById("contact-info").style.display = educationDisplayStatus;
-//       console.log("hiding");
-//   }
-// }
+//EDUCATION
+let educationDisplayStatus = "none";
+document.getElementById("education-info").style.display = educationDisplayStatus;
+
+document.getElementById("education").onclick = function() {
+  if (educationDisplayStatus == "none") {
+      educationDisplayStatus = "block";
+      document.getElementById("education-info").style.display = educationDisplayStatus;
+  }
+  else if (educationDisplayStatus == "block") {
+      educationDisplayStatus = "none";
+      document.getElementById("education-info").style.display = educationDisplayStatus;
+  }
+}
 
 // WORK EXPERIENCE
 let workexpDisplayStatus = "none";
@@ -40,11 +36,9 @@ document.getElementById("workexp").onclick = function() {
   if (workexpDisplayStatus == "none") {
       workexpDisplayStatus = "block";
       document.getElementById("work-info").style.display = workexpDisplayStatus;
-      console.log("showing workexp");
   }
   else if (workexpDisplayStatus == "block") {
       workexpDisplayStatus = "none";
       document.getElementById("work-info").style.display = workexpDisplayStatus;
-      console.log("hiding workexp");
   }
 }
